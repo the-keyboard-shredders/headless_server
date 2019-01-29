83f141a78209d805d605b612f78bfec08214b9bd
Pavel Machuca <pavelmachuca@hotmail.com>
2019-01-29 15:48:53 -0500

    re-did mutation to add article

--- server/models/article.js
+++ server/models/article.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const articleSchema = new Schema({
   title: String,
   content: String,
-  userId: String
+  userEmail: String
 });
 
 module.exports = mongoose.model('Article', articleSchema);
