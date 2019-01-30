d5e31e7b5049edf5b987abf6df0f33686af3925f
Pavel Machuca <pavelmachuca@hotmail.com>
2019-01-30 15:47:36 -0500

    added oauth, need to polish routes

--- server/index.js
+++ server/index.js
@@ -3,36 +3,45 @@ const expressGraphQL = require('express-graphql');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { Query } = mongoose;
+const cookieSession = require('cookie-session');
+const passport = require('passport')
+// const { Query } = mongoose;
+
+const schema = require('./schema/schema');
+const authRoutes = require('./routes/auth')
+const passportSetup = require('./passport/passport-setup');
+const secrets = require('../secrets')
 
 const app = express();
 const PORT = process.env.PORT || '4000';
+const db = process.env.MONGODB_URI || secrets.mongodb.dbURI;
 
-const db = process.env.MONGODB_URI || require('../secrets');
+app.use(cookieSession({
+  maxAge: 24 * 60 * 60 * 1000,
+  keys: [secrets.session.cookieKey]
+}));
 
-const schema = require('./schema/schema');
-
-// allow cross origin requests
-// app.use(cors());
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose
   .connect(db)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
-function isAuthenticated(req, res, next) {
-  // do any checks you want to in here
-  // CHECK THE USER STORED IN SESSION FOR A CUSTOM VARIABLE
-  // you can do this however you want with whatever variables you set up
-  if (req.user.authenticated) return next();
+app.use('/auth', authRoutes)
 
-  // IF A USER ISN'T LOGGED IN, THEN REDIRECT THEM SOMEWHERE
-  res.send('not authenticated');
+const authCheck = (req, res, next) => {
+  if (!req.user) {
+    res.redirect('/auth/login')
+  } else {
+    next()
+  }
 }
 
 app.use(
   '/',
-  isAuthenticated,
+  authCheck,
   cors(),
   bodyParser.json(),
   expressGraphQL({
---
+++ server/routes/auth.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const passport = require('passport');
+
+// /auth
+
+router.get(
+  '/login',
+  passport.authenticate('google', {
+    scope: ['profile']
+  })
+);
+
+router.get('/logout', (req, res, next) => {
+  req.logout()
+  res.redirect('/')
+});
+
+
+router.get('/google/redirect', passport.authenticate('google'), (req, res, next) => {
+  res.redirect('/')
+});
+
+module.exports = router;
